ca2fe2af81c5188b611a80fd9f5e59f249104cf2
felipe <felipegouveia.bfr@hotmail.com>
2023-06-21 11:09:17 -0300

    reqs 1 ao 7

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,39 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        name = instance.search(i)
+        if name["nome_do_arquivo"] == path_file:
+            return None
+
+    lines = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(result)
+
+    print(result)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if not len(instance):
+        print("Não há elementos")
+        return
+    removed = instance.dequeue()
+    print(f'Arquivo {removed["nome_do_arquivo"]} removido com sucesso')
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position))
+    except Exception:
+        print("Posição inválida", file=sys.stderr)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+        lines = file["linhas_do_arquivo"]
+
+        ocurrences = [
+            {"linha": i + 1}
+            for index in range(len(lines))
+            if word.lower() in lines[index].lower()
+        ]
+
+    if len(ocurrences) > 0:
+        search_result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": ocurrences,
+        }
+        result.append(search_result)
+
+    return result
 
 
 def search_by_word(word, instance):
